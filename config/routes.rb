ef345f06ad4cc9108057242015a9623c544595a9
Trevor Kimenye <kimenye@gmail.com>
2015-08-06 12:26:11 +0300

    Making root to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'visitors#index'
+  root to: '/admin'
 
   resources :articles, :categories
 end
