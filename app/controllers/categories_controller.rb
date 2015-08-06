bda1c314fd0f2e2297c367ff3715568b61e0a4f3
Trevor Kimenye <kimenye@gmail.com>
2015-08-06 12:42:11 +0300

    Fixed typo

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
   def index
     # TODO: Should we have an order index for display
     # Or use prediction.io
-    @categories = Categories.all
+    @categories = Category.all
   end
 
   # GET /categories/1
