37bc1bff69b2c50b1fb21697f7295565da8cbae6
Trevor Kimenye <kimenye@gmail.com>
2015-08-06 12:32:27 +0300

    Marked root as home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: '/admin'
+  
+  # root to: 'admin'
+  root :to => redirect('/admin')
 
   resources :articles, :categories
 end
